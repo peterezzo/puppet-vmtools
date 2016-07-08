1dc0cda4a63c0f460993a4990f6cb98cfbf75e66
Pete Ezzo <peter.ezzo@gmail.com>
2016-07-08 14:07:00 -0400

    new vmwaretools class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,22 @@
+# vmwaretools class simply installs open-vm-tools, included by hiera virtual
 class vmwaretools {
-    # only setup to use open-vm-tools at the moment
-    if $osfamily == 'Debian' {
-        package { 'open-vm-tools':
-              ensure => 'latest',
-        }
+  # i have a suspicion this script will blow up on RHEL but haven't actually tried it
+  case $::osfamily {
+    'Debian': {
+      package { 'open-vm-tools':
+        ensure => 'latest'
+      }
+    }
+    'RedHat': {
+      package { 'net-tools':
+        ensure => 'present'
+      }
+      package { 'open-vm-tools':
+        ensure  => 'latest',
+        require => Package['net-tools']
+      }
+    }
+    default: {
+      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
 }
