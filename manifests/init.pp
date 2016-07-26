81f2b23cd81deea7417723afdd3816e8a2cf96f4
Pete Ezzo <peter.ezzo@gmail.com>
2016-07-26 16:50:47 -0400

    migration from unified base class to split modules

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,25 +1,25 @@
-# vmwaretools class simply installs open-vm-tools, included by hiera virtual
+# vmwaretools class simply installs open-vm-tools if necessary
+# include everywhere and only run where virtual
 class vmwaretools {
-  require base  # dns/proxy setup in base
-
-  # i have a suspicion this script will blow up on RHEL but haven't actually tried it
-  case $::osfamily {
-    'Debian': {
-      package { 'open-vm-tools':
-        ensure => 'latest',
+  if $virtual == 'vmware' {
+    case $::osfamily {
+      'Debian': {
+        package { 'open-vm-tools':
+          ensure => 'latest',
+        }
       }
-    }
-    'RedHat': {
-      package { 'net-tools':
-        ensure => 'present',
+      'RedHat': {
+        package { 'net-tools':
+          ensure => 'present',
+        }
+        package { 'open-vm-tools':
+          ensure  => 'latest',
+          require => Package['net-tools']
+        }
       }
-      package { 'open-vm-tools':
-        ensure  => 'latest',
-        require => Package['net-tools']
+      default: {
+        fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
       }
-    }
-    default: {
-      fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
     }
   }
 }
